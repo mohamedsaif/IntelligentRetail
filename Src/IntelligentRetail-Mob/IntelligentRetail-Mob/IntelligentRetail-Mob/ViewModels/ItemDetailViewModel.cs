cbbff16ca03c7b5277a9d5b5686f61f739af8d88
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2018-07-12 09:31:44 +0400

    Adding initial Mobile Client

---
+++ Src/IntelligentRetail-Mob/IntelligentRetail-Mob/IntelligentRetail-Mob/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IntelligentRetail_Mob.Models;
+
+namespace IntelligentRetail_Mob.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
